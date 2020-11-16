7e6ded9de7fa24794f10a867ee772a34739bcfac
lewis150193 <lewis.williams@hmcts.net>
2020-11-16 10:07:23 +0000

    fix tests and code coverage

--- src/main/java/uk/gov/hmcts/reform/iataskconfiguration/config/SwaggerConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/iataskconfiguration/config/SwaggerConfiguration.java
@@ -1,4 +1,4 @@
-package uk.gov.hmcts.reform.ia.task.configuration.config;
+package uk.gov.hmcts.reform.iataskconfiguration.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,7 +7,7 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import uk.gov.hmcts.reform.ia.task.configuration.Application;
+import uk.gov.hmcts.reform.iataskconfiguration.Application;
 
 @Configuration
 @EnableSwagger2
