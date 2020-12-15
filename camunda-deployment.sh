2921bf3effb933a72f0d03dd9936fc5c7f996f80
Alejandro Baeza <alejandro.baeza@enginegroup.com>
2020-12-15 16:44:57 +0000

    clean up deployment script

--- camunda-deployment.sh
+++ camunda-deployment.sh
@@ -6,16 +6,14 @@
 ##
 ## deploys bpmn/dmn to camunda.
 
-
 BASEDIR=$(dirname "$0")
 SERVICE_TOKEN=$1
 
 for file in $BASEDIR/src/main/resources/*.bpmn $BASEDIR/src/main/resources/*.dmn; do
   if [ -f "$file" ]; then
-    curl --header "Content-Type: multipart/form-data" "ServiceAuthorization: ${SERVICE_TOKEN}" \
-      --request POST \
-      --form data=@$file \
-      "${CAMUNDA_URL}/deployment/create"
+    curl --silent --show-error ${CAMUNDA_URL}/deployment/create \
+      -H 'Content-Type: multipart/form-data' \
+      -H "ServiceAuthorization: ${SERVICE_TOKEN}" \
+      -F data=@$file
   fi
 done
-
