54af32a4b495d580567d29e254a21396913501bb
Alejandro Baeza <alejandro.baeza@enginegroup.com>
2020-12-15 16:34:03 +0000

    clean up deployment script

--- camunda-deployment.sh
+++ camunda-deployment.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
-## Usage: ./camunda-deployment
+## Usage: ./camunda-deployment [SERVICE_TOKEN]
 ##
 ## Options:
-##    - AUTHORIZATION: which is generated with the idam-service-token.
+##    - SERVICE_TOKEN: a service token for a whitelisted service in camunda which is generated with the idam-service-token.
 ##
 ## deploys bpmn/dmn to camunda.
 
-AUTHORIZATION= sh ${WA_KUBE_ENV_PATH}/scripts/actions/idam-service-token.sh
 
 BASEDIR=$(dirname "$0")
+SERVICE_TOKEN=$1
 
-for file in $BASEDIR/src/main/resources/*.bpmn $BASEDIR/src/main/resources/*.dmn
-do
-	if [ -f "$file" ]
-	then
-curl --header "Content-Type: multipart/form-data" "ServiceAuthorization: ${AUTHORIZATION}"\
-  --request POST \
-  --form data=@$file \
-  "${CAMUNDA_URL}/deployment/create"
+for file in $BASEDIR/src/main/resources/*.bpmn $BASEDIR/src/main/resources/*.dmn; do
+  if [ -f "$file" ]; then
+    curl --header "Content-Type: multipart/form-data" "ServiceAuthorization: ${SERVICE_TOKEN}" \
+      --request POST \
+      --form data=@$file \
+      "${CAMUNDA_URL}/deployment/create"
   fi
 done
+
