a64b44584972e5210b069322d05a20637e4371eb
david.crespo <david.crespo@hmcts.net>
2021-06-10 15:46:51 +0100

    add sendDirection to the completion dmn add unit tests

--- src/test/java/uk/gov/hmcts/reform/iataskconfiguration/dmn/CamundaTaskCompletionTest.java
+++ src/test/java/uk/gov/hmcts/reform/iataskconfiguration/dmn/CamundaTaskCompletionTest.java
@@ -62,9 +62,17 @@ class CamundaTaskCompletionTest extends DmnDecisionTableBaseUnitTest {
             ),
             Arguments.of(
                 "sendDirection",
-                singletonList(
+                asList(
                     Map.of(
                         "taskType", "reviewRespondentEvidence",
+                        "completionMode", "Auto"
+                    ),
+                    Map.of(
+                        "taskType", "reviewAdditionalHomeOfficeEvidence",
+                        "completionMode", "Auto"
+                    ),
+                    Map.of(
+                        "taskType", "reviewAdditionalAppellantEvidence",
                         "completionMode", "Auto"
                     )
                 )
@@ -238,7 +246,7 @@ class CamundaTaskCompletionTest extends DmnDecisionTableBaseUnitTest {
 
         //The purpose of this test is to prevent adding new rows without being tested
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
-        assertThat(logic.getRules().size(), is(15));
+        assertThat(logic.getRules().size(), is(17));
 
     }
 
