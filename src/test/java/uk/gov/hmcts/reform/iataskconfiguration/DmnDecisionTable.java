b88ab3f8855e96d13fae26ecc494d2844f412f46
david.crespo <david.crespo@hmcts.net>
2021-08-11 16:33:04 +0100

    add unit test for the cancellation wa dmn table

--- src/test/java/uk/gov/hmcts/reform/iataskconfiguration/DmnDecisionTable.java
+++ src/test/java/uk/gov/hmcts/reform/iataskconfiguration/DmnDecisionTable.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonValue;
 public enum DmnDecisionTable {
     WA_TASK_ALLOWED_DAYS_IA_ASYLUM("wa-task-allowed-days-ia-asylum", "wa-task-allowed-days-ia-asylum.dmn"),
     WA_TASK_CANCELLATION_IA_ASYLUM("wa-task-cancellation-ia-asylum", "wa-task-cancellation-ia-asylum.dmn"),
+    WA_TASK_CANCELLATION_WA_WACASETYPE("wa-task-cancellation-wa-wacasetype", "wa-task-cancellation-wa-wacasetype.dmn"),
     WA_TASK_COMPLETION_IA_ASYLUM("wa-task-completion-ia-asylum", "wa-task-completion-ia-asylum.dmn"),
     WA_TASK_CONFIGURATION_IA_ASYLUM("wa-task-configuration-ia-asylum", "wa-task-configuration-ia-asylum.dmn"),
     WA_TASK_INITIATION_IA_ASYLUM("wa-task-initiation-ia-asylum", "wa-task-initiation-ia-asylum.dmn"),
---
+++ src/test/java/uk/gov/hmcts/reform/iataskconfiguration/dmn/CamundaTaskWaCancellationTest.java
@@ -0,0 +1,43 @@
+package uk.gov.hmcts.reform.iataskconfiguration.dmn;
+
+import org.camunda.bpm.dmn.engine.DmnDecisionTableResult;
+import org.camunda.bpm.dmn.engine.impl.DmnDecisionTableImpl;
+import org.camunda.bpm.engine.variable.VariableMap;
+import org.camunda.bpm.engine.variable.impl.VariableMapImpl;
+import org.hamcrest.MatcherAssert;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import uk.gov.hmcts.reform.iataskconfiguration.DmnDecisionTableBaseUnitTest;
+
+import static java.util.Collections.EMPTY_LIST;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static uk.gov.hmcts.reform.iataskconfiguration.DmnDecisionTable.WA_TASK_CANCELLATION_WA_WACASETYPE;
+
+class CamundaTaskWaCancellationTest extends DmnDecisionTableBaseUnitTest {
+    @BeforeAll
+    public static void initialization() {
+        CURRENT_DMN_DECISION_TABLE = WA_TASK_CANCELLATION_WA_WACASETYPE;
+    }
+
+    @Test
+    void given_multiple_event_ids_should_evaluate_dmn() {
+        VariableMap inputVariables = new VariableMapImpl();
+        inputVariables.putValue("fromState", "some from state");
+        inputVariables.putValue("event", "some event id");
+        inputVariables.putValue("state", "some state");
+
+        DmnDecisionTableResult dmnDecisionTableResult = evaluateDmnTable(inputVariables);
+
+        MatcherAssert.assertThat(dmnDecisionTableResult.getResultList(), is(EMPTY_LIST));
+    }
+
+    @Test
+    void if_this_test_fails_needs_updating_with_your_changes() {
+        //The purpose of this test is to prevent adding new rows without being tested
+        DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
+        assertThat(logic.getInputs().size(), is(3));
+        assertThat(logic.getOutputs().size(), is(4));
+        assertThat(logic.getRules().size(), is(0));
+    }
+}
