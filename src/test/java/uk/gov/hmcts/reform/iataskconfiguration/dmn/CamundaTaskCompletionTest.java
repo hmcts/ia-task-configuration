5e95ff81e460d8221790da629febefe32791bf65
Augustine Onubeze <ikechukwu.onubeze@gmail.com>
2022-03-07 12:50:18 +0000

    RIA-5499: Remove completion rule for 'createHearingBundle' task
    - Since 'createHearingBundle' task no longer exists, we need to remove
      the rule for completing the task in task Completion DMN file.

--- src/test/java/uk/gov/hmcts/reform/iataskconfiguration/dmn/CamundaTaskCompletionTest.java
+++ src/test/java/uk/gov/hmcts/reform/iataskconfiguration/dmn/CamundaTaskCompletionTest.java
@@ -18,7 +18,6 @@ import java.util.stream.Stream;
 
 import static java.util.Arrays.asList;
 import static java.util.Collections.emptyList;
-import static java.util.Collections.emptyMap;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static uk.gov.hmcts.reform.iataskconfiguration.DmnDecisionTable.WA_TASK_COMPLETION_IA_ASYLUM;
@@ -229,16 +228,6 @@ class CamundaTaskCompletionTest extends DmnDecisionTableBaseUnitTest {
                 )
             ),
             Arguments.of(
-                "generateHearingBundle",
-                asList(
-                    Map.of(
-                        "taskType", "createHearingBundle",
-                        "completionMode", "Auto"
-                    ),
-                    emptyMap()
-                )
-            ),
-            Arguments.of(
                 "recordAllocatedJudge",
                 asList(
                     Map.of(
@@ -324,7 +313,7 @@ class CamundaTaskCompletionTest extends DmnDecisionTableBaseUnitTest {
 
         //The purpose of this test is to prevent adding new rows without being tested
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
-        assertThat(logic.getRules().size(), is(24));
+        assertThat(logic.getRules().size(), is(23));
 
     }
 
