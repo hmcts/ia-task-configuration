f80f17fa7dfe7b3d83ed3fd55caff13534e59630
Simon Buckley <simon.buckley@hmcts.net>
2022-09-21 17:22:36 +0100

    RIA 5752 Global Search Changes Squashed

--- src/test/java/uk/gov/hmcts/reform/iataskconfiguration/dmn/CamundaTaskConfigurationTest.java
+++ src/test/java/uk/gov/hmcts/reform/iataskconfiguration/dmn/CamundaTaskConfigurationTest.java
@@ -64,7 +64,7 @@ class CamundaTaskConfigurationTest extends DmnDecisionTableBaseUnitTest {
     void if_this_test_fails_needs_updating_with_your_changes() {
         //The purpose of this test is to prevent adding new rows without being tested
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
-        assertThat(logic.getRules().size(), is(17));
+        assertThat(logic.getRules().size(), is(16));
     }
 
     @SuppressWarnings("checkstyle:indentation")
@@ -245,7 +245,7 @@ class CamundaTaskConfigurationTest extends DmnDecisionTableBaseUnitTest {
 
         assertEquals(Map.of(
             "name", "workType",
-            "value", "access-requests"
+            "value", "access_requests"
         ), workTypeResultList.get(0));
     }
 
