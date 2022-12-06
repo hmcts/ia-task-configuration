aa83fa925b8969c3eb0635004a6c8f0099e03fd5
kalachandrasekar1 <kala.chandrasekar@HMCTS.NET>
2022-12-06 15:08:51 +0000

    Rebase build issue fixed

--- src/test/java/uk/gov/hmcts/reform/iataskconfiguration/dmn/CamundaTaskConfigurationTest.java
+++ src/test/java/uk/gov/hmcts/reform/iataskconfiguration/dmn/CamundaTaskConfigurationTest.java
@@ -37,12 +37,33 @@ class CamundaTaskConfigurationTest extends DmnDecisionTableBaseUnitTest {
     public static void initialization() {
         CURRENT_DMN_DECISION_TABLE = WA_TASK_CONFIGURATION_IA_ASYLUM;
     }
+    @ParameterizedTest
+    @CsvSource({
+        "followUpNoticeOfChange"
+    })
+    void when_taskId_then_return_Access_Requests(String taskType) {
+        VariableMap inputVariables = new VariableMapImpl();
 
+        inputVariables.putValue("taskAttributes", Map.of("taskType", taskType));
+
+        DmnDecisionTableResult dmnDecisionTableResult = evaluateDmnTable(inputVariables);
+
+        List<Map<String, Object>> workTypeResultList = dmnDecisionTableResult.getResultList().stream()
+            .filter((r) -> r.containsValue("workType"))
+            .collect(Collectors.toList());
+
+        assertEquals(1, workTypeResultList.size());
+
+        assertEquals(Map.of(
+            "name", "workType",
+            "value", "decision_making_work"
+        ), workTypeResultList.get(0));
+    }
     @Test
     void if_this_test_fails_needs_updating_with_your_changes() {
         //The purpose of this test is to prevent adding new rows without being tested
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
-        assertThat(logic.getRules().size(), is(16));
+        assertThat(logic.getRules().size(), is(17));
     }
 
     @SuppressWarnings("checkstyle:indentation")
