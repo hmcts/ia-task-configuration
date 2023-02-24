52eab918ba9144be76c66d5d0f90812f6917bae5
DennisHoV1 <dennis.ho@hmcts.net>
2023-02-24 18:10:44 +0000

    add the auto completion for new task update the workType for new task

--- src/test/java/uk/gov/hmcts/reform/iataskconfiguration/dmn/CamundaTaskCompletionTest.java
+++ src/test/java/uk/gov/hmcts/reform/iataskconfiguration/dmn/CamundaTaskCompletionTest.java
@@ -362,6 +362,15 @@ class CamundaTaskCompletionTest extends DmnDecisionTableBaseUnitTest {
                 )
             ),
             Arguments.of(
+                "listCase",
+                List.of(
+                    Map.of(
+                        "taskType", "adaListCase",
+                        "completionMode", "Auto"
+                    )
+                )
+            ),
+            Arguments.of(
                 "sendDecisionAndReasons",
                 asList(
                     Map.of(
@@ -418,7 +427,7 @@ class CamundaTaskCompletionTest extends DmnDecisionTableBaseUnitTest {
 
         //The purpose of this test is to prevent adding new rows without being tested
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
-        assertThat(logic.getRules().size(), is(41));
+        assertThat(logic.getRules().size(), is(42));
 
 
     }
