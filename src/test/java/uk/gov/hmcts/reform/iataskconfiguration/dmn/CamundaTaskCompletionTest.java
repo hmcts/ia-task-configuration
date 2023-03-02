28c8530c40626affcf1d3fa996211b401b41d46d
ThomasKKC <thomas.cheong@version1.com>
2023-03-02 15:34:04 +0000

    RIA-6828 Added ada decideAnApplication task

--- src/test/java/uk/gov/hmcts/reform/iataskconfiguration/dmn/CamundaTaskCompletionTest.java
+++ src/test/java/uk/gov/hmcts/reform/iataskconfiguration/dmn/CamundaTaskCompletionTest.java
@@ -358,6 +358,10 @@ class CamundaTaskCompletionTest extends DmnDecisionTableBaseUnitTest {
                         "taskType", "editListing",
                         "completionMode", "Auto"
                     ),
+                    Map.of(
+                        "taskType", "adaEditListing",
+                        "completionMode", "Auto"
+                    ),
                     Collections.emptyMap()
                 )
             ),
@@ -389,16 +393,6 @@ class CamundaTaskCompletionTest extends DmnDecisionTableBaseUnitTest {
                 asList(
                     Map.of(
                         "taskType", "prepareDecisionsAndReasons",
-                        "completionMode", "Auto"
-                    ),
-                    Collections.emptyMap()
-                )
-            ),
-            Arguments.of(
-                "editCaseListing",
-                asList(
-                    Map.of(
-                        "taskType", "editListing",
                         "completionMode", "Auto"
                     ),
                     Collections.emptyMap()
@@ -447,7 +441,7 @@ class CamundaTaskCompletionTest extends DmnDecisionTableBaseUnitTest {
 
         //The purpose of this test is to prevent adding new rows without being tested
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
-        assertThat(logic.getRules().size(), is(43));
+        assertThat(logic.getRules().size(), is(44));
 
 
     }
