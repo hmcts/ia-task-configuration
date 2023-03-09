68b45cb9d93ab387a9ef669d3af183193a7e2855
DennisHoV1 <106669367+DennisHoV1@users.noreply.github.com>
2023-03-09 11:02:02 +0000

    RIA-6829 add ADA list case when create hearing bundle (#277)
    * RIA-6829-add-ada-list-case-when-create-hearing-bundle
    
    * Add the new event of "listCase" to auto completion DMN file and update the unit test
    
    * Update the unit test size for task initiation
    
    ---------
    
    Co-authored-by: rajeshthuraiyur <48379561+rajeshthuraiyur@users.noreply.github.com>

--- src/test/java/uk/gov/hmcts/reform/iataskconfiguration/dmn/CamundaTaskCompletionTest.java
+++ src/test/java/uk/gov/hmcts/reform/iataskconfiguration/dmn/CamundaTaskCompletionTest.java
@@ -284,6 +284,10 @@ class CamundaTaskCompletionTest extends DmnDecisionTableBaseUnitTest {
                         "taskType", "caseSummaryHearingBundleStartDecision",
                         "completionMode", "Auto"
                     ),
+                    Map.of(
+                        "taskType", "adaCaseSummaryHearingBundleStartDecision",
+                        "completionMode", "Auto"
+                    ),
                     Collections.emptyMap()
                 )
             ),
@@ -371,7 +375,8 @@ class CamundaTaskCompletionTest extends DmnDecisionTableBaseUnitTest {
                     Map.of(
                         "taskType", "adaListCase",
                         "completionMode", "Auto"
-                    )
+                    ),
+                    Collections.emptyMap()
                 )
             ),
             Arguments.of(
@@ -445,7 +450,7 @@ class CamundaTaskCompletionTest extends DmnDecisionTableBaseUnitTest {
 
         //The purpose of this test is to prevent adding new rows without being tested
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
-        assertThat(logic.getRules().size(), is(45));
+        assertThat(logic.getRules().size(), is(46));
 
 
     }
