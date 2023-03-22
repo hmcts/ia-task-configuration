b935646e00b647865a14eda38ba6f6fe14c511d6
ThomasKKC <thomas.cheong@version1.com>
2023-03-22 12:34:11 +0000

    RIA-6472 Added markAsPaid task for CTSC

--- src/test/java/uk/gov/hmcts/reform/iataskconfiguration/dmn/CamundaTaskInitiationTest.java
+++ src/test/java/uk/gov/hmcts/reform/iataskconfiguration/dmn/CamundaTaskInitiationTest.java
@@ -1436,11 +1436,42 @@ class CamundaTaskInitiationTest extends DmnDecisionTableBaseUnitTest {
                                       + "      \"remissionDecision\":\"" + "partiallyApproved" + "\"\n"
                                       + "   }"
                                       + "}"),
-                singletonList(
+                List.of(
                     Map.of(
                         "taskId", "sendPaymentRequest",
                         "name", "Send Payment Request",
                         "workingDaysAllowed", 0,
+                        "processCategories", "caseProgression"
+                    ),
+                    Map.of(
+                        "taskId", "markAsPaid",
+                        "name", "Mark as Paid",
+                        "delayDuration", 12,
+                        "workingDaysAllowed", 14,
+                        "processCategories", "caseProgression"
+                    )
+                )
+            ),
+            Arguments.of(
+                "recordRemissionDecision",
+                null,
+                mapAdditionalData("{\n"
+                                      + "   \"Data\":{\n"
+                                      + "      \"remissionDecision\":\"" + "partiallyApproved" + "\"\n"
+                                      + "   }"
+                                      + "}"),
+                List.of(
+                    Map.of(
+                        "taskId", "sendPaymentRequest",
+                        "name", "Send Payment Request",
+                        "workingDaysAllowed", 0,
+                        "processCategories", "caseProgression"
+                    ),
+                    Map.of(
+                        "taskId", "markAsPaid",
+                        "name", "Mark as Paid",
+                        "delayDuration", 12,
+                        "workingDaysAllowed", 14,
                         "processCategories", "caseProgression"
                     )
                 )
@@ -1613,7 +1644,7 @@ class CamundaTaskInitiationTest extends DmnDecisionTableBaseUnitTest {
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
         assertThat(logic.getInputs().size(), is(8));
         assertThat(logic.getOutputs().size(), is(5));
-        assertThat(logic.getRules().size(), is(43));
+        assertThat(logic.getRules().size(), is(44));
     }
 
     public static Stream<Arguments> addendumScenarioProvider() {
