bf3ab56b2ed28bccfb34043f54e65a571fabf1a7
ThomasKKC <99262639+ThomasKKC@users.noreply.github.com>
2023-04-18 14:06:43 +0100

    RIA-7028 added paymentStatus additional data for recordRemissionDecision (#305)

--- src/test/java/uk/gov/hmcts/reform/iataskconfiguration/dmn/CamundaTaskInitiationTest.java
+++ src/test/java/uk/gov/hmcts/reform/iataskconfiguration/dmn/CamundaTaskInitiationTest.java
@@ -1577,6 +1577,17 @@ class CamundaTaskInitiationTest extends DmnDecisionTableBaseUnitTest {
                 )
             ),
             Arguments.of(
+                "recordRemissionDecision",
+                null,
+                mapAdditionalData("{\n"
+                                      + "   \"Data\":{\n"
+                                      + "      \"remissionDecision\":\"" + "partiallyApproved" + "\",\n"
+                                      + "      \"paymentStatus\":\"" + "Paid" + "\"\n"
+                                      + "   }"
+                                      + "}"),
+                emptyList()
+            ),
+            Arguments.of(
                 "unknownEvent",
                 null,
                 null,
@@ -1741,7 +1752,7 @@ class CamundaTaskInitiationTest extends DmnDecisionTableBaseUnitTest {
     void if_this_test_fails_needs_updating_with_your_changes() {
         //The purpose of this test is to prevent adding new rows without being tested
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
-        assertThat(logic.getInputs().size(), is(8));
+        assertThat(logic.getInputs().size(), is(9));
         assertThat(logic.getOutputs().size(), is(4));
         assertThat(logic.getRules().size(), is(44));
     }
