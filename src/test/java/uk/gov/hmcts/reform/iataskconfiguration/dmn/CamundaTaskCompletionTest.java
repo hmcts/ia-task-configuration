b9ba613cca0eeb646331bbc6c6f0ab9a04e88d96
rajeshthuraiyur <48379561+rajeshthuraiyur@users.noreply.github.com>
2023-05-11 20:43:51 +0100

    To deploy R4 changes in production (#315)
    * RIA-6344 Added new file task types for R4 (#285)
    
    * RIA-6344 Added new file task types for R4
    
    * RIA-6344 Added CVE-2022-45688
    
    * RIA-6344 Added TaskTypes unit test
    
    * RIA-6469 Added review remission application task (#284)
    
    * RIA-6474 Added assign a FTPA Judge task for CTSC (#286)
    
    * RIA-6474 Added assign a FTPA Judge task for CTSC
    
    * RIA-6474 Suppressed CVE-2022-1471
    
    * RIA-6473 Added List the case task for CTSC (#287)
    
    * RIA-6471 Added sendPaymentRequest task for CTSC (#290)
    
    * RIA-6472 Added markAsPaid task for CTSC (#291)
    
    * RIA-6967 remove some tasks from task types for R4 scope (#292)
    
    * RIA-6456 Court Specific Calendar - Due Date Configuration (#293)
    
    * RIA-6456 Court Specific Calendar - Due Date Configuration
    
    * RIA-6456 Added CVE-2023-28708 suppression
    
    * Revert "RIA-6456 Court Specific Calendar - Due Date Configuration (#293)" (#295)
    
    This reverts commit 595b979312ff7b7a9a21d7198d38cb22fd7abde6.
    
    * RIA-6457 Court Specific Calendar - Delay Until Calculation (#294)
    
    * RIA-6456 Court Specific Calendar - Due Date Configuration
    
    * RIA-6456 Added CVE-2023-28708 suppression
    
    * RIA-6457 Court Specific Calendar - Delay Until Calculation
    
    * RIA-6457 Updated court calendar and added priority configs
    
    * RIA-6457 Fixed dueDateOrigin date format (#296)
    
    * RIA-6457 Fixed dueDateOrigin date format
    
    * RIA-6457 Fixed unit test
    
    * RIA-6469 Added EUSS appeal type to reviewRemissionApplication task (#297)
    
    * RIA-6471 Updated hyperlink for sendPaymentRequest task (#298)
    
    * RIA-6472 Added due date skip non working days config (#299)
    
    * RIA-6473 Added listCaseWithoutHearingRequirements event for listTheCase task (#300)
    
    * RIA-6471 Changed auto complete event for sendPaymentRequest task (#301)
    
    * RIA-6472 changed markAsPaid category to followUpOverdue (#302)
    
    * RIA-6457 updated reconfigure values (#303)
    
    * RIA-6968 Update can reconfigure values (#304)
    
    * RIA-7028 added paymentStatus additional data for recordRemissionDecision (#305)
    
    * RIA-6456 Added extra calendar for testing (#306)
    
    * RIA-6456 Renamed calendar (#308)
    
    * RIA-7028 updated dueDateIntervalDays and editListingChangeHC (#309)
    
    * Revert "RIA-7028 updated dueDateIntervalDays and editListingChangeHC (#309)" (#310)
    
    This reverts commit b5a8764317a8a73ae48215fcc5793901b951e357.
    
    * RIA-6456 Updated due dates for various tasks (#311)
    
    * Update extra-non-working-day-calendar.json
    
    * Update suppressions.xml
    
    ---------
    
    Co-authored-by: ThomasKKC <99262639+ThomasKKC@users.noreply.github.com>

--- src/test/java/uk/gov/hmcts/reform/iataskconfiguration/dmn/CamundaTaskCompletionTest.java
+++ src/test/java/uk/gov/hmcts/reform/iataskconfiguration/dmn/CamundaTaskCompletionTest.java
@@ -308,6 +308,56 @@ class CamundaTaskCompletionTest extends DmnDecisionTableBaseUnitTest {
                 )
             ),
             Arguments.of(
+                "recordRemissionDecision",
+                asList(
+                    Map.of(
+                        "taskType", "reviewRemissionApplication",
+                        "completionMode", "Auto"
+                    ),
+                    Collections.emptyMap()
+                )
+            ),
+            Arguments.of(
+                "recordAllocatedJudge",
+                asList(
+                    Map.of(
+                        "taskType", "assignAFTPAJudge",
+                        "completionMode", "Auto"
+                    ),
+                    Collections.emptyMap()
+                )
+            ),
+            Arguments.of(
+                "listCase",
+                asList(
+                    Map.of(
+                        "taskType", "listTheCase",
+                        "completionMode", "Auto"
+                    ),
+                    Collections.emptyMap()
+                )
+            ),
+            Arguments.of(
+                "markAppealPaid",
+                asList(
+                    Map.of(
+                        "completionMode", "Auto",
+                        "taskType", "markAsPaid"
+                    ),
+                    Collections.emptyMap()
+                )
+            ),
+            Arguments.of(
+                "markPaymentRequestSent",
+                asList(
+                    Map.of(
+                        "completionMode", "Auto",
+                        "taskType", "sendPaymentRequest"
+                    ),
+                    Collections.emptyMap()
+                )
+            ),
+            Arguments.of(
                 "unknownEvent",
                 emptyList()
             )
@@ -330,7 +380,7 @@ class CamundaTaskCompletionTest extends DmnDecisionTableBaseUnitTest {
 
         //The purpose of this test is to prevent adding new rows without being tested
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
-        assertThat(logic.getRules().size(), is(22));
+        assertThat(logic.getRules().size(), is(27));
 
     }
 
