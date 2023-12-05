090d400ddd0efd82711979d8c7e5a8053129193e
nilay <nilay.patel@hmcts.net>
2023-12-05 17:29:47 +0000

    DIAC-112 adding retrigger

--- src/test/java/uk/gov/hmcts/reform/iataskconfiguration/dmn/CamundaTaskCancellationTest.java
+++ src/test/java/uk/gov/hmcts/reform/iataskconfiguration/dmn/CamundaTaskCancellationTest.java
@@ -270,7 +270,7 @@ class CamundaTaskCancellationTest extends DmnDecisionTableBaseUnitTest {
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
         assertThat(logic.getInputs().size(), is(3));
         assertThat(logic.getOutputs().size(), is(4));
-        assertThat(logic.getRules().size(), is(20));
+        assertThat(logic.getRules().size(), is(19));
 
     }
 }
