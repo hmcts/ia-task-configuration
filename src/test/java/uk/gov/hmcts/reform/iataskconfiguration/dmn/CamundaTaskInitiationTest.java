45fcc52973668da2b4b4ecc8c09e27968fa7cc4d
DennisHoV1 <dennis.ho@hmcts.net>
2023-12-13 15:57:23 +0000

    Update the rule based on the value of listCaseHearingCentre to determine whether is decisionWithoutHearing Update unit test

--- src/test/java/uk/gov/hmcts/reform/iataskconfiguration/dmn/CamundaTaskInitiationTest.java
+++ src/test/java/uk/gov/hmcts/reform/iataskconfiguration/dmn/CamundaTaskInitiationTest.java
@@ -1848,7 +1848,7 @@ class CamundaTaskInitiationTest extends DmnDecisionTableBaseUnitTest {
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
         assertThat(logic.getInputs().size(), is(13));
         assertThat(logic.getOutputs().size(), is(4));
-        assertThat(logic.getRules().size(), is(56));
+        assertThat(logic.getRules().size(), is(57));
     }
 
     public static Stream<Arguments> addendumScenarioProvider() {
