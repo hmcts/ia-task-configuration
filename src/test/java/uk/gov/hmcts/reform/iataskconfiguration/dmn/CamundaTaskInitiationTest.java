b140bc78774b87d12ec7d7dcd4c9956af2362109
Marcellofabbri <marcello.fabbri@hmcts.net>
2024-02-20 16:57:00 +0000

    RIA-8632 Delay CMR tasks (send and update) 2 days

--- src/test/java/uk/gov/hmcts/reform/iataskconfiguration/dmn/CamundaTaskInitiationTest.java
+++ src/test/java/uk/gov/hmcts/reform/iataskconfiguration/dmn/CamundaTaskInitiationTest.java
@@ -59,6 +59,11 @@ class CamundaTaskInitiationTest extends DmnDecisionTableBaseUnitTest {
             "delayUntilOrigin", LocalDate.now()
         );
 
+        Map<String,Object> delayFor2Days = Map.of(
+            "delayUntilIntervalDays", "2",
+            "delayUntilOrigin", LocalDate.now()
+        );
+
         return Stream.of(
             Arguments.of(
                 "applyForFTPAAppellant",
@@ -1675,6 +1680,7 @@ class CamundaTaskInitiationTest extends DmnDecisionTableBaseUnitTest {
                     Map.of(
                         "taskId", "cmrUpdated",
                         "name", "Update CMR notification",
+                        "delayUntil", delayFor2Days,
 
                         "processCategories", "caseProgression"
                     )
@@ -1726,6 +1732,7 @@ class CamundaTaskInitiationTest extends DmnDecisionTableBaseUnitTest {
                     Map.of(
                         "taskId", "cmrListed",
                         "name", "Send CMR notification",
+                        "delayUntil", delayFor2Days,
 
                         "processCategories", "caseProgression"
                     )
