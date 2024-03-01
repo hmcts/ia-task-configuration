682673e5fc1fd1fb36a186f3e8ce5c29d43281aa
nilay <nilay.patel@hmcts.net>
2024-03-01 15:45:43 +0000

    DIAC-112 making reviewAddendumEvidence JUDICIAL user

--- src/test/java/uk/gov/hmcts/reform/iataskconfiguration/dmn/CamundaTaskConfigurationTest.java
+++ src/test/java/uk/gov/hmcts/reform/iataskconfiguration/dmn/CamundaTaskConfigurationTest.java
@@ -72,7 +72,7 @@ class CamundaTaskConfigurationTest extends DmnDecisionTableBaseUnitTest {
     void if_this_test_fails_needs_updating_with_your_changes() {
         //The purpose of this test is to prevent adding new rows without being tested
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
-        assertThat(logic.getRules().size(), is(32));
+        assertThat(logic.getRules().size(), is(33));
     }
 
     @SuppressWarnings("checkstyle:indentation")
