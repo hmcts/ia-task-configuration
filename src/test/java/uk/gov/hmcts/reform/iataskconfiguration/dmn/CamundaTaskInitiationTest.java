7145c3369aa3731a6fa3fae8eff569f9a92e6135
Aliveni Choppa <aliveni.choppa1@hmcts.net>
2025-02-10 18:41:52 +0000

    DIAC-859 WA task for AiP remission appeals

--- src/test/java/uk/gov/hmcts/reform/iataskconfiguration/dmn/CamundaTaskInitiationTest.java
+++ src/test/java/uk/gov/hmcts/reform/iataskconfiguration/dmn/CamundaTaskInitiationTest.java
@@ -2742,9 +2742,9 @@ class CamundaTaskInitiationTest extends DmnDecisionTableBaseUnitTest {
     void if_this_test_fails_needs_updating_with_your_changes() {
         //The purpose of this test is to prevent adding new rows without being tested
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
-        assertThat(logic.getInputs().size(), is(26));
+        assertThat(logic.getInputs().size(), is(27));
         assertThat(logic.getOutputs().size(), is(4));
-        assertThat(logic.getRules().size(), is(88));
+        assertThat(logic.getRules().size(), is(90));
     }
 
     public static Stream<Arguments> addendumScenarioProvider() {
