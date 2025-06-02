7758f8d301f67206c2985287bb865f12d2ef5bc0
Claudio Andrei <72282843+erclaudio@users.noreply.github.com>
2025-06-02 10:15:35 +0100

    DIAC-1096 and 1098 fix after merge (#467)
    * fixes after merging error
    
    * fix test
    
    * test fix
    
    * fix test and merging
    
    ---------
    
    Co-authored-by: erclaudio <claudio.andrei@icloud.com>

--- src/test/java/uk/gov/hmcts/reform/iataskconfiguration/dmn/CamundaTaskInitiationTest.java
+++ src/test/java/uk/gov/hmcts/reform/iataskconfiguration/dmn/CamundaTaskInitiationTest.java
@@ -2930,7 +2930,7 @@ class CamundaTaskInitiationTest extends DmnDecisionTableBaseUnitTest {
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
         assertThat(logic.getInputs().size(), is(28));
         assertThat(logic.getOutputs().size(), is(4));
-        assertThat(logic.getRules().size(), is(105));
+        assertThat(logic.getRules().size(), is(106));
     }
 
     public static Stream<Arguments> addendumScenarioProvider() {
