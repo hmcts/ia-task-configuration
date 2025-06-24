5d5c11883f2ed7fcbbe881353487505d69dd5b41
Sabah Irfan <sabah.irfan@gmail.com>
2025-06-24 12:46:30 +0100

    Add unit tests

--- src/test/java/uk/gov/hmcts/reform/iataskconfiguration/dmn/CamundaTaskInitiationTest.java
+++ src/test/java/uk/gov/hmcts/reform/iataskconfiguration/dmn/CamundaTaskInitiationTest.java
@@ -3239,7 +3239,7 @@ class CamundaTaskInitiationTest extends DmnDecisionTableBaseUnitTest {
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
         assertThat(logic.getInputs().size(), is(28));
         assertThat(logic.getOutputs().size(), is(4));
-        assertThat(logic.getRules().size(), is(120));
+        assertThat(logic.getRules().size(), is(121));
     }
 
     public static Stream<Arguments> addendumScenarioProvider() {
