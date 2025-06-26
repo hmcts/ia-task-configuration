52f5b7595099c0d694a8785f62d2921ac455d5d5
erclaudio <claudio.andrei@icloud.com>
2025-06-26 14:09:33 +0100

    update test

--- src/test/java/uk/gov/hmcts/reform/iataskconfiguration/dmn/CamundaTaskCompletionTest.java
+++ src/test/java/uk/gov/hmcts/reform/iataskconfiguration/dmn/CamundaTaskCompletionTest.java
@@ -553,7 +553,7 @@ class CamundaTaskCompletionTest extends DmnDecisionTableBaseUnitTest {
 
         //The purpose of this test is to prevent adding new rows without being tested
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
-        assertThat(logic.getRules().size(), is(63));
+        assertThat(logic.getRules().size(), is(67));
     }
 
 
