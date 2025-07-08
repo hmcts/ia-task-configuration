461228aa430487f25405a0517cf4a7e2417c697b
tiagreaney <tiagreaney@gmail.com>
2025-07-08 14:04:41 +0100

    update test

--- src/test/java/uk/gov/hmcts/reform/iataskconfiguration/dmn/CamundaTaskInitiationTest.java
+++ src/test/java/uk/gov/hmcts/reform/iataskconfiguration/dmn/CamundaTaskInitiationTest.java
@@ -3024,7 +3024,7 @@ class CamundaTaskInitiationTest extends DmnDecisionTableBaseUnitTest {
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
         assertThat(logic.getInputs().size(), is(28));
         assertThat(logic.getOutputs().size(), is(4));
-        assertThat(logic.getRules().size(), is(103));
+        assertThat(logic.getRules().size(), is(107));
     }
 
     public static Stream<Arguments> addendumScenarioProvider() {
