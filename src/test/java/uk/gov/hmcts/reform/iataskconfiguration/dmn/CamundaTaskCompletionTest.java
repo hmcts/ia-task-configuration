0be1398a30c2fb836e13b90a29159a406fec8eda
erclaudio <claudio.andrei@icloud.com>
2025-07-24 17:05:50 +0100

    tests fixed

--- src/test/java/uk/gov/hmcts/reform/iataskconfiguration/dmn/CamundaTaskCompletionTest.java
+++ src/test/java/uk/gov/hmcts/reform/iataskconfiguration/dmn/CamundaTaskCompletionTest.java
@@ -474,6 +474,10 @@ class CamundaTaskCompletionTest extends DmnDecisionTableBaseUnitTest {
                         "taskType", "listTheCase",
                         "completionMode", "Auto"
                     ),
+                    Map.of(
+                        "taskType", "detainedListTheCase",
+                        "completionMode", "Auto"
+                    ),
                     Collections.emptyMap()
                 )
             ),
@@ -568,7 +572,7 @@ class CamundaTaskCompletionTest extends DmnDecisionTableBaseUnitTest {
 
         //The purpose of this test is to prevent adding new rows without being tested
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
-        assertThat(logic.getRules().size(), is(70));
+        assertThat(logic.getRules().size(), is(71));
     }
 
 
