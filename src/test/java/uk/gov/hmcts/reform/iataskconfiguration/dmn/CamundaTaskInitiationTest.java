5190ecfb121a0a994aff626626a53b03e836c058
erclaudio <claudio.andrei@icloud.com>
2025-07-25 13:55:55 +0100

    test fix

--- src/test/java/uk/gov/hmcts/reform/iataskconfiguration/dmn/CamundaTaskInitiationTest.java
+++ src/test/java/uk/gov/hmcts/reform/iataskconfiguration/dmn/CamundaTaskInitiationTest.java
@@ -3422,7 +3422,7 @@ class CamundaTaskInitiationTest extends DmnDecisionTableBaseUnitTest {
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
         assertThat(logic.getInputs().size(), is(28));
         assertThat(logic.getOutputs().size(), is(4));
-        assertThat(logic.getRules().size(), is(143));
+        assertThat(logic.getRules().size(), is(145));
 
     }
 
