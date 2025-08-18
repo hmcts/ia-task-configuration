a7a4653bea6007736f747e6414676ee027ac6d64
Sabah Irfan <sabah.irfan@gmail.com>
2025-08-18 13:52:05 +0100

    Fix the initiation dmn file and tests

--- src/test/java/uk/gov/hmcts/reform/iataskconfiguration/dmn/CamundaTaskInitiationTest.java
+++ src/test/java/uk/gov/hmcts/reform/iataskconfiguration/dmn/CamundaTaskInitiationTest.java
@@ -4370,7 +4370,7 @@ class CamundaTaskInitiationTest extends DmnDecisionTableBaseUnitTest {
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
         assertThat(logic.getInputs().size(), is(28));
         assertThat(logic.getOutputs().size(), is(4));
-        assertThat(logic.getRules().size(), is(167));
+        assertThat(logic.getRules().size(), is(168));
 
     }
 
