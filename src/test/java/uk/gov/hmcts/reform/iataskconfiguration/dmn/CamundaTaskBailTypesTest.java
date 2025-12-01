3fd03e8bba2f80ec0dc9ff44eed793d6ee11ad44
nilay <nilay.patel@solirius.com>
2025-12-01 17:29:07 +0000

    fixes to tests that were showing false positives

--- src/test/java/uk/gov/hmcts/reform/iataskconfiguration/dmn/CamundaTaskBailTypesTest.java
+++ src/test/java/uk/gov/hmcts/reform/iataskconfiguration/dmn/CamundaTaskBailTypesTest.java
@@ -1,12 +1,15 @@
 package uk.gov.hmcts.reform.iataskconfiguration.dmn;
 
+import java.util.stream.Stream;
 import org.camunda.bpm.dmn.engine.DmnDecisionTableResult;
 import org.camunda.bpm.dmn.engine.impl.DmnDecisionTableImpl;
 import org.camunda.bpm.engine.variable.VariableMap;
 import org.camunda.bpm.engine.variable.impl.VariableMapImpl;
-import org.hamcrest.MatcherAssert;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import uk.gov.hmcts.reform.iataskconfiguration.DmnDecisionTableBaseUnitTest;
 
 import java.util.List;
@@ -14,6 +17,7 @@ import java.util.Map;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static uk.gov.hmcts.reform.iataskconfiguration.DmnDecisionTable.WA_TASK_TYPES_IA_BAIL;
 
 class CamundaTaskBailTypesTest extends DmnDecisionTableBaseUnitTest {
@@ -23,48 +27,52 @@ class CamundaTaskBailTypesTest extends DmnDecisionTableBaseUnitTest {
         CURRENT_DMN_DECISION_TABLE = WA_TASK_TYPES_IA_BAIL;
     }
 
-    static List<Map<String, Object>> expectedTaskTypes = List.of(
-        Map.of(
-            "taskTypeId", "processBailApplication",
-            "taskTypeName", "Process application"
-        ),
-        Map.of(
-            "taskTypeId", "reviewInterpreterFlag",
-            "taskTypeName", "Review interpreter booking"
-        ),
-        Map.of(
-            "taskTypeId", "noticeOfChange",
-            "taskTypeName", "Notice of Change"
-        ),
-        Map.of(
-            "taskTypeId", "followUpBailSummary",
-            "taskTypeName", "Follow up Home Office summary"
-        ),
-        Map.of(
-            "taskTypeId", "reviewAdditionalEvidence",
-            "taskTypeName", "Review additional evidence"
-        ),
-        Map.of(
-            "taskTypeId", "uploadSignedDecision",
-            "taskTypeName", "Upload signed decision"
-        ),
-        Map.of(
-            "taskTypeId", "listForFurtherReview",
-            "taskTypeName", "List for further review"
-        ),
-        Map.of(
-            "taskTypeId", "postHearingRecord",
-            "taskTypeName", "Post hearing – attendees, duration and recording"
-        )
-    );
+    static Stream<Arguments> scenarioProvider() {
+        List<Map<String, String>> taskTypes = List.of(
+            Map.of(
+                "taskTypeId", "processBailApplication",
+                "taskTypeName", "Process application"
+            ),
+            Map.of(
+                "taskTypeId", "reviewInterpreterFlag",
+                "taskTypeName", "Review interpreter booking"
+            ),
+            Map.of(
+                "taskTypeId", "noticeOfChange",
+                "taskTypeName", "Notice of Change"
+            ),
+            Map.of(
+                "taskTypeId", "followUpBailSummary",
+                "taskTypeName", "Follow up Home Office summary"
+            ),
+            Map.of(
+                "taskTypeId", "reviewAdditionalEvidence",
+                "taskTypeName", "Review additional evidence"
+            ),
+            Map.of(
+                "taskTypeId", "uploadSignedDecision",
+                "taskTypeName", "Upload signed decision"
+            ),
+            Map.of(
+                "taskTypeId", "listForFurtherReview",
+                "taskTypeName", "List for further review"
+            ),
+            Map.of(
+                "taskTypeId", "postHearingRecord",
+                "taskTypeName", "Post hearing – attendees, duration and recording"
+            )
+        );
+        return taskTypes.stream().map(Arguments::of);
+    }
 
-    @Test()
-    void should_evaluate_dmn_return_all_task_type_fields() {
+    @ParameterizedTest
+    @MethodSource("scenarioProvider")
+    void should_evaluate_dmn_return_all_task_type_fields(Map<String, Object> expectedTaskType) {
         VariableMap inputVariables = new VariableMapImpl();
         DmnDecisionTableResult dmnDecisionTableResult = evaluateDmnTable(inputVariables);
+        List<Map<String, Object>> resultList = dmnDecisionTableResult.getResultList();
 
-        MatcherAssert.assertThat(dmnDecisionTableResult.getResultList(), is(expectedTaskTypes));
-
+        assertTrue(resultList.contains(expectedTaskType));
     }
 
     @Test
