a8a41c86cf2c86ddb734c1c5d830a8d955690e23
tiagreaney <tiagreaney@gmail.com>
2026-01-08 11:41:09 +0000

    DIAC-1972 Initiate 'Print and send appeal decision and FTPA form' WA task

--- src/test/java/uk/gov/hmcts/reform/iataskconfiguration/dmn/CamundaTaskInitiationTest.java
+++ src/test/java/uk/gov/hmcts/reform/iataskconfiguration/dmn/CamundaTaskInitiationTest.java
@@ -4388,7 +4388,7 @@ class CamundaTaskInitiationTest extends DmnDecisionTableBaseUnitTest {
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
         assertThat(logic.getInputs().size(), is(31));
         assertThat(logic.getOutputs().size(), is(4));
-        assertThat(logic.getRules().size(), is(199));
+        assertThat(logic.getRules().size(), is(201));
     }
 
     public static Stream<Arguments> addendumScenarioProvider() {
