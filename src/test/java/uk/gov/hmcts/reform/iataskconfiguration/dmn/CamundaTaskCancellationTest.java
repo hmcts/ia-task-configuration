49e5fea220ff2c2b53635eb8ca621eaf202100ea
david-gimelle <david.gimelle@gmail.com>
2026-04-16 07:10:46 +0100

    change to Listing

--- src/test/java/uk/gov/hmcts/reform/iataskconfiguration/dmn/CamundaTaskCancellationTest.java
+++ src/test/java/uk/gov/hmcts/reform/iataskconfiguration/dmn/CamundaTaskCancellationTest.java
@@ -352,6 +352,17 @@ class CamundaTaskCancellationTest extends DmnDecisionTableBaseUnitTest {
                         "action", "Reconfigure"
                     )
                 )
+            ),
+            Arguments.of(
+                null,
+                "listCase",
+                "listing",
+                singletonList(
+                    Map.of(
+                        "action", "Cancel",
+                        "processCategories", "caseProgression"
+                    )
+                )
             )
         );
     }
@@ -378,7 +389,7 @@ class CamundaTaskCancellationTest extends DmnDecisionTableBaseUnitTest {
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
         assertThat(logic.getInputs().size(), is(3));
         assertThat(logic.getOutputs().size(), is(4));
-        assertThat(logic.getRules().size(), is(29));
+        assertThat(logic.getRules().size(), is(30));
 
     }
 }
