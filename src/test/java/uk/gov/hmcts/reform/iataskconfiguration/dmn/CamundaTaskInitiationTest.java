9aa58048c994d9914372b9bbf56e527814306aa2
tiagreaney <tiagreaney@gmail.com>
2026-04-30 09:59:51 +0100

    change respondToQuery to queries and queries_stf work type

--- src/test/java/uk/gov/hmcts/reform/iataskconfiguration/dmn/CamundaTaskInitiationTest.java
+++ src/test/java/uk/gov/hmcts/reform/iataskconfiguration/dmn/CamundaTaskInitiationTest.java
@@ -4900,7 +4900,7 @@ class CamundaTaskInitiationTest extends DmnDecisionTableBaseUnitTest {
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
         assertThat(logic.getInputs().size(), is(33));
         assertThat(logic.getOutputs().size(), is(4));
-        assertThat(logic.getRules().size(), is(202));
+        assertThat(logic.getRules().size(), is(203));
     }
 
     public static Stream<Arguments> addendumScenarioProvider() {
