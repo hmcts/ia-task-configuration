630b22d498efec5ce92220ce167e1997bc690ca8
tiagreaney <tiagreaney@gmail.com>
2026-09-14 12:15:43 +0100

    update test

--- src/test/java/uk/gov/hmcts/reform/iataskconfiguration/dmn/CamundaTaskInitiationTest.java
+++ src/test/java/uk/gov/hmcts/reform/iataskconfiguration/dmn/CamundaTaskInitiationTest.java
@@ -3310,9 +3310,9 @@ class CamundaTaskInitiationTest extends DmnDecisionTableBaseUnitTest {
     void if_this_test_fails_needs_updating_with_your_changes() {
         //The purpose of this test is to prevent adding new rows without being tested
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
-        assertEquals(33, logic.getInputs().size());
+        assertEquals(34, logic.getInputs().size());
         assertEquals(4, logic.getOutputs().size());
-        assertEquals(205, logic.getRules().size());
+        assertEquals(207, logic.getRules().size());
     }
 
     @ParameterizedTest
